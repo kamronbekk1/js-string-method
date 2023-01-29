eeecee1a49fc68d9268c539cb9b821d40eecb88c
kamronbekk1 <kamronbekkamolov41@gmail.com>
2023-01-29 10:49:08 +0500

    string method

---
+++ main.js
@@ -0,0 +1,45 @@
+//TASK 1
+// 1) let user = prompt('What is your Name ?') // ismi faqatgina a/A harfi bilan boshlanadigan odamlarni kirsin.
+// 2) let balance = prompt('How much do you have money ?') // Puli 10 000 mingdan kam bo'lmaganlarni kiriting.
+// 3) let account = prompt('Code ?') // 7777 bo'lishi kerak;
+// 4) let cash = prompt('How much do you want to cash ?') //qancha puli qoldi va qancha naqt pul oldi log da chiqarilsin;
+
+//TASK 2
+// let txt = "Lorem ipsum@ dolor sit@ amet consectetur, adipisicing@ elit. Aliquam lorem molestias consequuntur! alexandr .Atque@ lorem adipisci delectus@ lorem facere accusantium! Aliquam voluptate lorem aut asperiores@ fugiat sunt lorem unde necessitatibus@ delectus!"z
+// 1) txt dagi hamma @ larni o'chirib chiqush;
+// 2) txt dagi hamma lorem so'zini 'Hello' ga o'zgartirasizlar;
+// 3) txt dagi hamma lorem so'zini nechi marta qaytariliyapti sanaysizlar;
+// 4) txt dagi Alexandr so'zini topib uni bosh harfini katta qilasizlar;
+
+
+
+//TASK1
+//1
+//let user = prompt('What is your Name ?')
+//console.log(user);
+
+//2
+//let user = ('Alex')
+//console.log(user);
+//let balance = prompt('puli 10 000 kamlar kirmasin')
+//console.log(balance);
+//let account = prompt('kodni kiriting')
+//let cash = prompt('qancha pulingiz qoldi?')
+
+
+
+
+//TASK 2
+//1
+//let txt = "Lorem ipsum@ dolor sit@ amet consectetur, adipisicing@ elit. Aliquam lorem molestias consequuntur! alexandr .Atque@ lorem adipisci delectus@ lorem facere accusantium! Aliquam voluptate lorem aut asperiores@ fugiat sunt lorem unde necessitatibus@ delectus!"
+//console.log(txt.replace(/@/gi, ''));
+//console.log(txt.replace(/lorem/gi, 'Hello'));
+//console.log(txt.replace("alexandr", 'Alexandr'));
+//console.log(txt.matchAll(/lorem/gi));
+
+
+
+
+
+
+
